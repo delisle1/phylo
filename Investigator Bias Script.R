1ded541b99e150914afffd05333dec97e1dd4de1
delisle1 <102507801+delisle1@users.noreply.github.com>
2022-08-26 11:04:45 +0200

    Add files via upload

--- Investigator Bias Script.R
+++ Investigator Bias Script.R
@@ -16,6 +16,20 @@ for (i in tetrapodsubtrees){print((i$edge.length/tetrapodtree$edge.length))
 for (i in tetrapodsubtrees){print(length(i$tip.label)/length(tetrapodsubtrees$tip.label)
 #Convert tree to tibble
 tetrapodtreetibble<-as_tibble(tetrapodtree)
+## Read cladistic dataset
+data <- ape::read.nexus.data("Tetrapods_202_42_ord_PAUP_runfile-cleaned.nex")
+## Combining it into a matrix
+data <- do.call(rbind, data)
+## Calculate the distance matrix (by.col = by characters)
+distances <- dispRity::char.diff(data,
+                                 method = "mord", # See the reference in the ?dispRity::char.diff manual for the mord method (and other options)
+                                 by.col = TRUE) # by.col = for the characters (not the taxa) in a NEXUS matrix
+#Calculate means of a subsets
+distances1<-submatrix(distances, c('3','5','6'),c('3','5','6'))
+mean(distances1)
+#perform for each subset of characters identified in PAUP. 
+#Create null SESdistribution 
+nulldist<-replicate(1000,table(sample(nrow(distances), 15, replace = TRUE)))
 #Load imbalance and bias data
 read.csv('tetrapoddata.csv')
 #Attach SES values to tibble
@@ -27,3 +41,6 @@ tetrapodmultipleregression<-with(tetrapoddata, lm(Imbalance~Index+SESmcd))
 summary(tetrapodmultipleregression)
 #Perform Spearman's rank test
 cor.test(tetrapoddata$Index,tetrapoddata$SESmcd, method = 'spearman', exact = FALSE)
+#Generate randomized tree
+simtree$edge.length<-sample(tettree$edge.length)
+
